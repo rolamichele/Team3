2a599903519fc471315e4aeb74551a2aa5e50af8
shahd <shahdmohamed25112005@gmail.com>
2026-06-15 02:08:54 +0300

    Update Package.php

--- Event/Routes/Package.php
+++ Event/Routes/Package.php
@@ -2,24 +2,23 @@
 require_once "../controller/PackageController.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
-$path = $_SERVER['PATH_INFO'];
 
-if ($_SERVER['REQUEST_METHOD'] == "GET" && $path == '/package') {
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
     getAllPackages();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['id']) && $path == '/package') {
+if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['id']) ) {
     getPackageById($_GET['id']);
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/package') {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     createPackage($data);
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "PUT" && $path == '/package' && isset($_GET['id'])) {
+if ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($_GET['id'])) {
     updatePackage($_GET['id'], $data);
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "DELETE" && $path == '/package' && isset($_GET['id'])) {
+if ($_SERVER['REQUEST_METHOD'] == "DELETE" && isset($_GET['id'])) {
     deletePackage($_GET['id']);
 }
