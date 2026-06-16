116ba4fbed48589afc4c910139a9d21c63f25af2
mariamosama <mariemosama43@gmail.com>
2026-06-16 04:24:00 +0300

    UserAuth

--- Event/helper/Jwt.php
+++ Event/helper/Jwt.php
@@ -1,1 +1,35 @@
-
+<?php
+require_once '../helper/response.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+function GenerateToken($user)
+{
+        $payload=[
+            "iat"=>time(),
+            "exp"=>time()+ 3600,
+            "user_id"=>$user['UserID'],
+            "role" =>$user['Role']  
+            ];
+     return JWT::encode($payload,"B0RN0Jx6muUoyGJGmahlRiQJ6mpNXEDQShyHT8bCbYp", 'HS256');
+}
+function verifyToken(){
+    $headers=getallheaders();
+    $token=$headers['Authorization']??'';
+    if (!$token){
+        response(401,"token is required");
+    }
+    $token=str_replace("Bearer ","",$token);
+    try{
+        $decoded=JWT::decode($token,new Key("B0RN0Jx6muUoyGJGmahlRiQJ6mpNXEDQShyHT8bCbYp",'HS256'));
+        return $decoded; 
+    }catch(Exception $e)
+    {
+        response(401, $e->getMessage() );
+    }
+}
+function require_admin($decodedToken){
+    if ($decodedToken->role!=="admin"){
+        response(403,"you are not authorized to access this resource");
+    }
+}
+?>
--- Event/helper/response.php
+++ Event/helper/response.php
@@ -1,1 +1,9 @@
-
+<?php
+function response($code,$message,$data=null)
+{
+    header('Content-Type: application/json');
+    http_response_code($code);
+    echo json_encode(["message"=>$message,"data"=>$data]);
+    exit;
+}
+?>
