66358730bd726f701cd3c1ed29bf9fa2c90f2931
Ammar Khaled <ammarkhaled2005@gmail.com>
2026-06-16 06:20:20 +0300

    Resolve merge conflict

--- Event/helper/Jwt.php
+++ Event/helper/Jwt.php
@@ -1,76 +1,43 @@
 <?php
-<<<<<<< HEAD
+
+require_once '../helper/response.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
+$secret_key = "B0RN0Jx6muUoyGJGmah1RiQJ6mpNXEDQShyHT8bCbYp";
 
 function generateToken($user)
 {
+    global $secret_key;
+
     $payload = [
         "iat" => time(),
         "exp" => time() + 3600,
-        "user_id" => $user['id'],
-        "role" => $user['role']
+        "user_id" => $user['UserID'],
+        "role" => $user['Role']
     ];
 
-    return JWT::encode(
-        $payload,
-         "B0RN0Jx6muUoyGJGmah1RiQJ6mpNXEDQShyHT8bCbYp",
-        "HS256"
-    );
+    return JWT::encode($payload, $secret_key, "HS256");
 }
 
-function VerifyToken()
+function verifyToken()
 {
+    global $secret_key;
+
     $headers = getallheaders();
     $token = $headers['Authentication'] ?? '';
 
     if (!$token) {
-        response(401, "token is requird");
+        response(401, "Token is required");
     }
 
     $token = str_replace("Bearer ", "", $token);
 
-    try {
-        $decoded = JWT::decode(
-            $token,
-            new Key("YOUR_SECRET_KEY", "HS256")
-        );
-
-        return $decoded;
-    } catch (Exception $e) {
-        response(401, "invalid token");
-    }
-}
-=======
-require_once '../helper/response.php';
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-function GenerateToken($user) {
-    global $secret_key;
->>>>>>> 0a3f366d58d2ab304b870c3a05135a62d1654014
-
-   $payload = [
-        "iat"     => time(),
-        "exp"     => time() + 3600,
-        "user_id" => $user['UserID'],
-        "role"    => $user['Role']
-    ];
- return JWT::encode($payload, $secret_key, "HS256");
-}
-function VerifyToken() {
-    global $secret_key;
-    $headers = getallheaders();
-    $token   = $headers['Authentication'] ?? '';
-    if (!$token) {
-        response(401, 'Token is required.');
-    }
-    $token = str_replace("Bearer ", "", $token);
     try {
         $decoded = JWT::decode($token, new Key($secret_key, "HS256"));
         return $decoded;
     } catch (Exception $e) {
-        response(401, 'Invalid token.');
+        response(401, "Invalid token");
     }
 }
--- Event/helper/response.php
+++ Event/helper/response.php
@@ -1,28 +1,18 @@
 <?php
-<<<<<<< HEAD
-
-function response($code,$message,$data=null)
+function response($code, $message, $data = null)
 {
     header("Content-Type: application/json");
-
     http_response_code($code);
 
     echo json_encode([
-        "message"=>$message,
-        "data"=>$data
+        "message" => $message,
+        "data" => $data
     ]);
 
     exit;
 }
-=======
-function response($code , $message , $data=null){
-    header("Content-Type: application/json");
-    http_response_code($code);
-    echo json_encode(["message" => $message , "data" => $data]);
-    exit;
-}
-function getRequestBody() {
+
+function getRequestBody()
+{
     return json_decode(file_get_contents("php://input"), true);
 }
->>>>>>> 0a3f366d58d2ab304b870c3a05135a62d1654014
-
