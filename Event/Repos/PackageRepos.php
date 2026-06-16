197c29a96c041eeca063b95a92e4ba2a332c0c45
shahd <shahdmohamed25112005@gmail.com>
2026-06-16 05:19:47 +0300

    Update PackageRepos.php

--- Event/Repos/PackageRepos.php
+++ Event/Repos/PackageRepos.php
@@ -1,1 +1,34 @@
+<?php
+require_once '../config/DB.php';
+function getAllPackages() {
+    global $pdo;
+    $getAll = $pdo->prepare("SELECT * FROM `Package` WHERE ActivityStat = 1");
+    $getAll->execute();
+    return $getAll->fetchAll();
+}
 
+function getPackageById($id) {
+    global $pdo;
+    $getById = $pdo->prepare("SELECT * FROM `Package` WHERE PackageID = ?");
+    $getById->execute([$id]);
+    return $getById->fetch();
+}
+
+function createPackage($title, $description, $price, $activity_stat = 1) {
+    global $pdo;
+    $create = $pdo->prepare("INSERT INTO `Package` (Title, Description, Price, ActivityStat) VALUES (?, ?, ?, ?)");
+    $create->execute([$title, $description, $price, $activity_stat]);
+    return $pdo->lastInsertId();
+}
+
+function updatePackage($id, $title, $description, $price, $activity_stat) {
+    global $pdo;
+    $update = $pdo->prepare("UPDATE `Package` SET Title = ?, Description = ?, Price = ?, ActivityStat = ? WHERE PackageID = ?");
+    return $update->execute([$title, $description, $price, $activity_stat, $id]);
+}
+
+function deletePackage($id) {
+    global $pdo;
+    $delete = $pdo->prepare("DELETE FROM `Package` WHERE PackageID = ?");
+    return $delete->execute([$id]);
+}
