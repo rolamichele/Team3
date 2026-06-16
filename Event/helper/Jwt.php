f36f87745eb91d675216515899b10cf708d18e72
Ammar Khaled <ammarkhaled2005@gmail.com>
2026-06-16 05:55:22 +0300

    Add vendor API (search, filter, pagination, cache)

--- Event/helper/Jwt.php
+++ Event/helper/Jwt.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -42,4 +43,34 @@ function VerifyToken()
         response(401, "invalid token");
     }
 }
+=======
+require_once '../helper/response.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+function GenerateToken($user) {
+    global $secret_key;
+>>>>>>> 0a3f366d58d2ab304b870c3a05135a62d1654014
 
+   $payload = [
+        "iat"     => time(),
+        "exp"     => time() + 3600,
+        "user_id" => $user['UserID'],
+        "role"    => $user['Role']
+    ];
+ return JWT::encode($payload, $secret_key, "HS256");
+}
+function VerifyToken() {
+    global $secret_key;
+    $headers = getallheaders();
+    $token   = $headers['Authentication'] ?? '';
+    if (!$token) {
+        response(401, 'Token is required.');
+    }
+    $token = str_replace("Bearer ", "", $token);
+    try {
+        $decoded = JWT::decode($token, new Key($secret_key, "HS256"));
+        return $decoded;
+    } catch (Exception $e) {
+        response(401, 'Invalid token.');
+    }
+}
--- Event/helper/response.php
+++ Event/helper/response.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 
 function response($code,$message,$data=null)
 {
@@ -13,4 +14,15 @@ function response($code,$message,$data=null)
 
     exit;
 }
+=======
+function response($code , $message , $data=null){
+    header("Content-Type: application/json");
+    http_response_code($code);
+    echo json_encode(["message" => $message , "data" => $data]);
+    exit;
+}
+function getRequestBody() {
+    return json_decode(file_get_contents("php://input"), true);
+}
+>>>>>>> 0a3f366d58d2ab304b870c3a05135a62d1654014
 
