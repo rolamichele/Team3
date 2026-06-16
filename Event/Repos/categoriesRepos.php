b72497cf3a3bed6da002078b154312082c1cf2c8
osamaa005 <mohamedosamaali005@gmail.com>
2026-06-17 01:48:51 +0300

    Update categoriesRepos.php

--- Event/Repos/categoriesRepos.php
+++ Event/Repos/categoriesRepos.php
@@ -1,41 +1,58 @@
 <?php
-require_once '../config/DB.php'
+
+require_once '../config/DB.php';
+
 function getAllcategories(){
+
     global $pdo;
-    $getAll = $pdo->prepare("SELECT * FROM `categories` WHERE 1");
+
+    $getAll = $pdo->prepare("SELECT * FROM categories WHERE 1");
+
     $getAll->execute();
+
     return $getAll->fetchAll();
+
 }
 
+
 function getcategoriesById($id) {
+
     global $pdo;
-    $getById = $pdo->prepare("SELECT * FROM categories` WHERE CategoryID = ?");
+
+    $getById = $pdo->prepare("SELECT * FROM categories WHERE CategoryID = ?");
+
     $getById->execute([$id]);
+
     return $getById->fetch();
 }
 
 
- function update_categorie($id, $name, $description){
-      global $pdo;
-    $sql = "UPDATE Category
+function update_categorie($id, $name, $description){
+
+    global $pdo;
+
+    $sql = "UPDATE categories
             SET Name = ?, Description = ?
             WHERE CategoryID = ?";
 
-    $stmt = $this->conn->prepare($sql);
+    $stmt = $pdo->prepare($sql);
 
     return $stmt->execute([
         $name,
         $description,
         $id
     ]);
-   
+
 }
 
- function delete($id)
-{
-    $sql = "DELETE FROM Categories WHERE CategoryID = ?";
 
-    $stmt = $this->conn->prepare($sql);
+function delete($id)
+{
+    global $pdo;
+
+    $sql = "DELETE FROM categories WHERE CategoryID = ?";
+
+    $stmt = $pdo->prepare($sql);
 
     return $stmt->execute([$id]);
 }
