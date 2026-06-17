8532254dc2417e99ff183e77a40ba6b32b3e1155
osamaa005 <mohamedosamaali005@gmail.com>
2026-06-17 22:13:33 +0300

    Update categories.php

--- Event/Routes/categories.php
+++ Event/Routes/categories.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once "../Controllers/CategoriesController.php";
+require_once "../Controller/categoriescontroller.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
+$data = json_decode(file_get_contents("php://input"), true);
 
 if ($method == "GET" && isset($_GET['id'])) {
     getById($_GET['id']);
@@ -11,10 +12,10 @@ else if ($method == "GET") {
     getAll();
 }
 else if ($method == "POST") {
-    insert();
+    insert($data);
 }
 else if ($method == "PUT" && isset($_GET['id'])) {
-    update($_GET['id']);
+    update($_GET['id'],$data);
 }
 else if ($method == "DELETE" && isset($_GET['id'])) {
     deleteCategory($_GET['id']);
