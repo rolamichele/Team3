5821798178e99ce40beafe0f799af992b34304fc
shahd <shahdmohamed25112005@gmail.com>
2026-06-17 06:16:00 +0300

    Update DB.php

--- Event/config/DB.php
+++ Event/config/DB.php
@@ -2,11 +2,11 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$database = "eventmanagement";
+$database = "test2";
 
 try {
-    $connection = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $err) {
     echo json_encode(['status' => 'error', 'message' => 'Connection failed: ' . $err->getMessage()]);
     exit;
