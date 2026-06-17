5aae257269d165f899e61723a736eb57ff12ee85
shahd <shahdmohamed25112005@gmail.com>
2026-06-17 06:15:20 +0300

    Update Package.php

--- Event/Routes/Package.php
+++ Event/Routes/Package.php
@@ -7,23 +7,18 @@ $data = json_decode(file_get_contents("php://input"), true);
 if ($method == "GET" && isset($_GET['id'])) {
     GET_PACKAGE_BY_ID();
 }
-
 else if ($method == "GET") {
     GET_PACKAGES();
 }
-
 else if ($method == "POST" && isset($_GET['review'])) {
     ADD_REVIEW();
 }
-
 else if ($method == "POST") {
     CREATE_PACKAGE();
 }
-
 else if ($method == "PUT" && isset($_GET['id'])) {
     UPDATE_PACKAGE();
 }
-
 else if ($method == "DELETE" && isset($_GET['id'])) {
     DELETE_PACKAGE();
 }
